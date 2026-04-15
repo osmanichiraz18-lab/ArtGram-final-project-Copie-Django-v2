790d66088367e5fc631a4099a2fc600734ee0114
Chiraz Osmani <osmani_chiraz.fs@univ-boumerdes.dz>
2026-04-15 14:14:28 +0200

    i add the model user and artwork

--- artworks/models.py
+++ artworks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Artwork(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to='artworks/')
+    artist = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='artworks')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.title
--- users/models.py
+++ users/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profiles/', blank=True, null=True)
+    is_artist = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.username
+    
+    def get_artworks_count(self):
+        return self.artworks.count()
+    
+    def get_likes_received_count(self):
+        total = 0
+        for artwork in self.artworks.all():
+            total += artwork.likes.count()
+        return total
